47351253d608c9b3a2a33a6cba3637e38a8d3d22
deshmukhakash55 <deshmukhakash55@gmail.com>
2019-07-15 09:37:29 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchresultsComponent } from './components/searchresults/searchresults.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContentComponent,
+    FooterComponent,
+    SearchresultsComponent
   ],
   imports: [
     BrowserModule,
